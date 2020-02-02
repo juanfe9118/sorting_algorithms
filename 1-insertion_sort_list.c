b0d4335a659ee36d4a05c3b38e9902029737c3ba
juanfe9118 <1168@holbertonschool.com>
2020-02-01 23:20:14 -0500

    Add insertion sort with algorithm complexity

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,1 +1,42 @@
 #include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending
+ * order using the insertion sort algorithm
+ * @list: the doubly linked list to sort
+ * Return: nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *tail = (*list)->next;
+    listint_t *b_i = NULL, *ins = NULL;
+
+    if (*list == NULL)
+        return;
+    while (tail != NULL)
+    {
+        ins = tail;
+        b_i = tail->prev;/*check if it is already in order*/
+        while (b_i != NULL)
+        {
+            if (ins->n < b_i->n)
+            {
+                ins->prev = b_i->prev;
+                b_i->next = ins->next;
+                b_i->prev = ins;
+                ins->next = b_i;
+                if (ins->prev != NULL)
+                    ins->prev->next = ins;
+                else
+                    *list = ins;
+                if (b_i->next != NULL)
+                    b_i->next->prev = b_i;
+                b_i = ins->prev;
+                print_list(*list);
+            }
+            else
+                break;
+        }
+        tail = tail->next;
+    }
+}
